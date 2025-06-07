4b1070dea8d34abec287d3816400509a71c0f7a3
maeka <maek.alekdawi@gmail.com>
2025-06-07 13:33:23 +0200

    Frontend connection

--- src/main/java/com/example/clickgame/ClickgameApplication.java
+++ src/main/java/com/example/clickgame/ClickgameApplication.java
@@ -2,10 +2,23 @@ package com.example.clickgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ClickgameApplication {
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://falling-game-zeta.vercel.app/")
+                        .allowedMethods("*");
+            }
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(ClickgameApplication.class, args);
     }
